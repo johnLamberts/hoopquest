4b8cafd8cf24f8d950e5f897712cd30947a767d6
John Lambert Asis <69611656+johnLamberts@users.noreply.github.com>
2024-07-18 12:07:47 +0800

    Delete server/dist directory

--- server/dist/src/constants/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authorizationRoles = void 0;
-exports.authorizationRoles = {
-    user: "user",
-    admin: "admin",
-};
-//# sourceMappingURL=auth.js.map
